f5e0a6073c7007534ee15511b238b3b8335d5cc0
David Levin <72104334+davidlevinwork@users.noreply.github.com>
2022-07-30 12:29:05 +0300

    Update my_executive.py

--- my_executive.py
+++ my_executive.py
@@ -19,13 +19,13 @@ class QLearningExecutor(Executor):
     def __init__(self, policy_file):
         super(QLearningExecutor, self).__init__()
 
-        self.lr = 0.8  # The 'alpha' from Bellman's equation
-        self.epsilon = 1  # The indicator for epsilon greedy method
-        self.gamma = 0.6  # The 'gamma' from Bellman's equation
+        self.lr = 0.8                           # The 'alpha' from Bellman's equation
+        self.epsilon = 1                        # The indicator for epsilon greedy method
+        self.gamma = 0.6                        # The 'gamma' from Bellman's equation
 
-        self.q_table = None  # Q Table
-        self.state_space = []  # State Space in the given domain
-        self.action_space = []  # Action Space in the given domain
+        self.q_table = None                     # Q Table
+        self.state_space = []                   # State Space in the given domain
+        self.action_space = []                  # Action Space in the given domain
         self.policy_file = policy_file
         self.policy_file_helper = policy_file + "_HELPER"
 
@@ -34,9 +34,9 @@ class QLearningExecutor(Executor):
     """
     def initialize(self, services):
         self.services = services
-        self.initialize_Q_table()  # Init/Import policy file
-        self.print_Q_table_to_file()  # Create the policy file
-        self.print_Q_table_helper_to_file()  # Create the policy helper file
+        self.initialize_Q_table()                   # Init/Import policy file
+        self.print_Q_table_to_file()                # Create the policy file
+        self.print_Q_table_helper_to_file()         # Create the policy helper file
 
     """
     Function role is to initialize the Q table with zeros: 
@@ -44,16 +44,16 @@ class QLearningExecutor(Executor):
     # COLUMNS = the possible actions will be the columns in the Q table.
     """
     def initialize_Q_table(self):
-        for action in self.services.parser.actions:  # Save all the possible actions of the given domain
+        for action in self.services.parser.actions:                 # Save all the possible actions of the given domain
             self.action_space.append(action)
 
-        for object in self.services.parser.objects:  # Save all the possible objects of the given domain
+        for object in self.services.parser.objects:                 # Save all the possible objects of the given domain
             if "food" not in object and "person" not in object:
                 self.state_space.append(object)
 
         if os.path.isfile((os.getcwd() + "/" + self.policy_file)):  # If the policy file exist - import it
             self.q_table = self.merge_Q_table()
-        else:  # If there isn't policy file - create it
+        else:                                                       # If there isn't policy file - create it
             self.q_table = np.zeros((len(self.state_space), len(self.action_space)))
 
     """
@@ -61,8 +61,8 @@ class QLearningExecutor(Executor):
     """
     def merge_Q_table(self):
         values = []
-        lines = open(self.policy_file).read().splitlines()  # Import all the value from the policy file
-        for i, line in enumerate(lines):  # Save the values
+        lines = open(self.policy_file).read().splitlines()              # Import all the value from the policy file
+        for i, line in enumerate(lines):                                # Save the values
             values.append(np.fromstring(line, dtype=float, sep=' '))
 
         matrix = np.array(values)
@@ -83,12 +83,12 @@ class QLearningExecutor(Executor):
     The difference between the original policy file, is that in this file we will print the columns and rows values.
     """
     def print_Q_table_helper_to_file(self):
-        t_headers = list(self.action_space)  # Create the columns names
+        t_headers = list(self.action_space)             # Create the columns names
         t_headers.insert(0, "State")
 
         table = PrettyTable([header for header in t_headers])
 
-        for i, n_row in enumerate(self.q_table):  # Import the values from the policy file
+        for i, n_row in enumerate(self.q_table):        # Import the values from the policy file
             row = list([col for col in n_row])
             row.insert(0, self.state_space[i])
             table.add_row([col for col in row])
@@ -102,7 +102,7 @@ class QLearningExecutor(Executor):
     """
     @staticmethod
     def get_reward(action):
-        if "pick-food" in action[0]:  # The agent is in the state of the food
+        if "pick-food" in action[0]:                    # The agent is in the state of the food
             return 10
         else:
             return -1
@@ -111,15 +111,15 @@ class QLearningExecutor(Executor):
         if self.services.goal_tracking.reached_all_goals():
             return None
 
-        exp_exp_tradeoff = random.uniform(0, 1)  # Exploitation & Exploration tradeoff
+        exp_exp_tradeoff = random.uniform(0, 1)         # Exploitation & Exploration tradeoff
 
-        if exp_exp_tradeoff > self.epsilon:  # Agent => Exploitation
+        if exp_exp_tradeoff > self.epsilon:             # Agent => Exploitation
             action = self.get_action_by_exploitation()
-        else:  # Agent => Exploration
+        else:                                           # Agent => Exploration
             action = self.get_action_by_exploration()
 
-        self.print_Q_table_to_file()  # Print the Q-table to the policy file
-        self.print_Q_table_helper_to_file()  # Print the Q-table to the policy helper file
+        self.print_Q_table_to_file()                    # Print the Q-table to the policy file
+        self.print_Q_table_helper_to_file()             # Print the Q-table to the policy helper file
         self.epsilon *= 0.8
 
         return action
@@ -143,16 +143,16 @@ class QLearningExecutor(Executor):
         best_action = None
         action_index = None
         random.shuffle(actions)
-        for action in actions:  # Iterate over all the valid actions
+        for action in actions:                                              # Iterate over all the valid actions
             if first:
                 first = False
                 best_action = action
                 act = action.replace("(", "").replace(")", "").split(" ")[0]
                 action_index = self.action_space.index(act)
                 action_value = self.q_table[state_index, action_index]
-            act = action.replace("(", "").replace(")", "").split(" ")[0]  # Extract the action name
+            act = action.replace("(", "").replace(")", "").split(" ")[0]    # Extract the action name
             matrix_column = self.action_space.index(act)
-            if action_value <= self.q_table[state_index, matrix_column]:  # Save the action by the biggest Q value
+            if action_value <= self.q_table[state_index, matrix_column]:    # Save the action by the biggest Q value
                 best_action = action
                 action_index = matrix_column
                 action_value = self.q_table[state_index, matrix_column]
